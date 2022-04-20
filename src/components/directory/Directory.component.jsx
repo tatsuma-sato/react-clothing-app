242fa4bc03d73639636f2dd50c860b9d956c8779
tatsuma-sato <t.tsuma.s@gmail.com>
2022-04-19 19:16:41 -0700

    updated directory and item

---
+++ src/components/directory/Directory.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CategoryItem from "../category-item/CategoryItem.component";
+import "./Directory.styles.scss";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
